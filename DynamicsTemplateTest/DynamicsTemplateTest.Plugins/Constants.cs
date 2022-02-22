e9a7576cf4c2056b0c5e1b0062cc70126237910f
rohitnanda016 <rohitnanda016@gmail.com>
2022-02-22 17:20:34 +0530

    Add project files.

---
+++ DynamicsTemplateTest/DynamicsTemplateTest.Plugins/Constants.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DynamicsTemplateTest.Plugins
+{
+    // pluginContext input parameters
+    public class Constants
+    {
+        // parameters
+        public const string InputParameterTarget = "Target";
+        public const string OutputParameterResult = "Result";
+        public const string PreImage = "PreImage";
+    }
+}
